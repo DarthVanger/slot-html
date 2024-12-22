bb1941e7686a829dc2c5e7a37f09dbb33bdbe099
DarthVanger <darthvanger@gmail.com>
2024-12-22 10:18:22 +0200

    Implement ways win logic

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const numRows = 2
 export const numCols = 3
+export const symbols = [0, 1]
--- src/machine.js
+++ src/machine.js
@@ -1,4 +1,4 @@
-const numRows = 2
+import { numRows, numCols, symbols } from './config.js'
 
 let reels = [
   [0, 0, 0, 0, 1, 1, 1, 1],
@@ -35,20 +35,46 @@ export function spin() {
   reels = reels.map(reel => spinReel(reel))
   const screen = getScreen(reels)
 
-  const wins = [
-    {
-      positions: [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}],
-    },
-    {
-      positions: [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
-    },
-    {
-      positions: [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}],
-    },
-    {
-      positions: [{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 1}],
-    }
-  ]
+  const wins = calculateWins(screen)
 
   return {screen, wins, reels}
+}
+
+function calculateWinForSymbol(symbol, screen) {
+  const positions = []
+  for (let x = 0; x < numCols; x++) {
+    for (let y = 0; y < numRows; y++) {
+      if (screen[x][y] === symbol) {
+        positions.push({x,y})
+      }
+    }
+  }
+
+  let numInRow = 0
+  for (let x = 0; x < numCols; x++) {
+    const reelHasSymbol = positions.find(p => p.x === x)
+    if (reelHasSymbol) {
+      numInRow++
+    }
+  }
+
+  if (numInRow >= 3) {
+    return {
+      positions,
+      numInRow
+    }
+  }
+}
+
+export function calculateWins(screen) {
+  const wins = []
+
+  for (const symbol of symbols) {
+    const win = calculateWinForSymbol(symbol, screen)
+    if (win) {
+      wins.push(win)
+    }
+  }
+  
+  return wins
 }
